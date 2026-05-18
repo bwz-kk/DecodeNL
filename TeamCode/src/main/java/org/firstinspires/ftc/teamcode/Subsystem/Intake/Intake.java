4f57babb7e6c706ea614ab7ead50efb4805ac9ef
Heitor Hillesheim dos Santos <heitorhilles@gmail.com>
2026-05-18 18:15:29 -0300

    intake power handled through the periodic, cleaner approach

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Intake/Intake.java
@@ -5,22 +5,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
+
     DcMotor Intake1;
+    double currentPower = 0;
 
     public Intake(HardwareMap hardwareMap) {
         Intake1 = hardwareMap.get(DcMotor.class, IntakeConstants.HMIntake1);
     }
 
-    public void TurnOnIntake(){
-        Intake1.setPower(IntakeConstants.TurnOn);
-}
+    public void TurnOnIntake() {
+        currentPower = IntakeConstants.TurnOn;
+    }
 
     public void TurnIntakeOff() {
-        Intake1.setPower(IntakeConstants.TurnOff);
+        currentPower = IntakeConstants.TurnOff;
     }
 
     @Override
-    public void periodic(){
-
+    public void periodic() {
+        Intake1.setPower(currentPower);
     }
 }
